86ac4264d66dd1c79a6e0f018cdb3f79c0f3f420
zxcmnb102 <zxcmnb05@naver.com>
2022-10-18 11:11:20 +0900

    ADD: ProfileScreen

--- app/src/main/java/com/example/todiary/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/todiary/ui/profile/ProfileScreen.kt
@@ -1,36 +1,164 @@
 package com.example.todiary.ui.profile
 
+import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.todiary.R
+import com.example.todiary.ui.theme.LightGray
 
 @Composable
-fun ProfileScreen() {
-    Box(
+fun ProfileScreen(
+    clickList: () -> Unit,
+    viewModel: ProfileViewModel
+) {
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Gray)
+            .padding(horizontal = 16.dp, vertical = 16.dp)
     ) {
-        Text(
-            text = "프로필",
-            style = MaterialTheme.typography.h1,
-            textAlign = TextAlign.Center,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.Center)
+        TopAppBar()
+        Spacer(modifier = Modifier.height(16.dp))
+        MyDiary(clickList, viewModel)
+        Spacer(modifier = Modifier.height(16.dp))
+        Setting()
+        Spacer(modifier = Modifier.height(16.dp))
+        Divider(
+            modifier = Modifier
+                .height(1.dp)
+                .fillMaxWidth()
+                .padding()
+        )
+        DarkMode()
+        Divider(
+            modifier = Modifier
+                .height(1.dp)
+                .fillMaxWidth()
+                .padding()
         )
     }
 }
 
-@Preview
 @Composable
-fun Preview() {
-    ProfileScreen()
+fun TopAppBar() {
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+        Text(text = "더보기", fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
+    }
+}
+
+@Composable
+fun MyDiary(clickList: () -> Unit, viewModel: ProfileViewModel) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min)
+            .padding(horizontal = 40.dp)
+            .clip(RoundedCornerShape(2.dp))
+            .border(width = 0.5.dp, color = Color.Black, shape = RoundedCornerShape(10.dp)),
+        Arrangement.SpaceEvenly
+    ) {
+        WrittenDiary(clickList, viewModel)
+        Divider(
+            color = LightGray, modifier = Modifier
+                .fillMaxHeight()
+                .width(1.dp)
+                .padding(vertical = 24.dp)
+        )
+        BookMarkDiary(clickList, viewModel)
+    }
+}
+
+@SuppressLint("StateFlowValueCalledInComposition")
+@Composable
+fun WrittenDiary(clickList: () -> Unit, viewModel: ProfileViewModel) {
+    val size = if (viewModel.postList.value.isNotEmpty()) viewModel.postList.value.size else 0
+
+    Column(
+        modifier = Modifier
+            .padding(24.dp)
+            .clickable {
+                clickList()
+            },
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "작성한 일기", fontSize = 14.sp, fontWeight = FontWeight.SemiBold)
+        Text(text = "$size", fontSize = 12.sp, fontWeight = FontWeight.SemiBold)
+    }
+}
+
+@SuppressLint("StateFlowValueCalledInComposition")
+@Composable
+fun BookMarkDiary(clickList: () -> Unit, viewModel: ProfileViewModel) {
+    val size =
+        if (viewModel.bookmarkList.value.isNotEmpty()) viewModel.bookmarkList.value.size else 0
+
+    Column(
+        modifier = Modifier
+            .padding(24.dp)
+            .clickable {
+                clickList()
+            }, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "즐겨찾기", fontSize = 14.sp, fontWeight = FontWeight.SemiBold)
+        Text(text = "$size", fontSize = 12.sp, fontWeight = FontWeight.SemiBold)
+    }
+}
+
+@Composable
+fun Setting() {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(text = "설정", fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
+    }
+}
+
+@Composable
+fun DarkMode() {
+    val context = LocalContext.current
+
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .height(IntrinsicSize.Min)
+        .clickable {
+            Toast
+                .makeText(context, "추후 추가 예정", Toast.LENGTH_SHORT)
+                .show()
+        }) {
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = "다크모드",
+            color = Color.Black,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(painter = painterResource(id = R.drawable.ic_right), contentDescription = null)
+        }
+
+    }
 }
--- app/src/main/java/com/example/todiary/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/todiary/ui/profile/ProfileViewModel.kt
@@ -1,9 +1,34 @@
 package com.example.todiary.ui.profile
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todiary.data.database.entity.DiaryEntity
+import com.example.todiary.data.repository.DiaryRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor(): ViewModel() {
+class ProfileViewModel @Inject constructor(
+    private val diaryRepository: DiaryRepository
+) : ViewModel() {
+    private val _diaryList = MutableStateFlow<List<DiaryEntity>>(emptyList())
+    val postList = _diaryList.asStateFlow()
+
+    private val _bookmarkList = MutableStateFlow<List<DiaryEntity>>(emptyList())
+    val bookmarkList = _bookmarkList.asStateFlow()
+
+    fun getDiaryList() {
+        viewModelScope.launch(Dispatchers.IO) {
+            diaryRepository.getAll().distinctUntilChanged().collect() { it ->
+                _diaryList.value = it
+                _bookmarkList.value = it.filter { it.postLike == 1 }
+            }
+        }
+    }
 }
