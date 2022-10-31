779ef410299ef791f01b1a24b91e3cef206baa72
zxcmnb102 <zxcmnb05@naver.com>
2022-10-31 11:09:39 +0900

    ADD: CustomListener

---
+++ app/src/main/java/com/example/todiary/util/CustomListener.kt
@@ -0,0 +1,27 @@
+package com.example.todiary.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+
+@Composable
+fun LifecycleListener(OnEvent: (event: Lifecycle.Event) -> Unit) {
+    val eventHandler = rememberUpdatedState(newValue = OnEvent)
+    val lifecycleOwner = rememberUpdatedState(newValue = LocalLifecycleOwner.current)
+
+    DisposableEffect(lifecycleOwner.value) {
+        val lifecycle = lifecycleOwner.value.lifecycle
+        val observer = LifecycleEventObserver { source, event ->
+            eventHandler.value(event)
+        }
+
+        lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycle.removeObserver(observer)
+        }
+    }
+}
